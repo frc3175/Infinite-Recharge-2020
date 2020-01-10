066f7ad13151235dd4834296813c7afb73d62ccc
Ian Sjögren <34076834+Ianshogren@users.noreply.github.com>
2020-01-10 16:51:34 -0500

    Added Shooter.java
    * Added Shooter method
    *RobotMap - Added Shooter IDs and Shooter Speed

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import frc.robot.lib.RobotMap;
+
+public class Shooter {
+
+    private TalonSRX shooter0, shooter1;
+
+
+    public void shooter(boolean pressed) {
+
+        shooter0 = new TalonSRX(RobotMap.shooter0ID);
+        shooter1 = new TalonSRX(RobotMap.shooter1ID);
+
+        if(pressed) {
+            shooter0.set(ControlMode.PercentOutput, RobotMap.shooterSpeed);
+            shooter1.set(ControlMode.PercentOutput, -RobotMap.shooterSpeed);
+        } else {
+            shooter0.set(ControlMode.PercentOutput, 0);
+            shooter1.set(ControlMode.PercentOutput, 0);
+        }
+
+
+
+
+    }
+
+
+
+}
