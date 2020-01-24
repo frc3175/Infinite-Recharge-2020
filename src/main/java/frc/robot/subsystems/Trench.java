27ca97798d94206be36167896f042ec313855a67
IsabellaHaladjian <59668551+IsabellaHaladjian@users.noreply.github.com>
2020-01-24 17:46:47 -0500

    Added Servo to Trench.java

--- src/main/java/frc/robot/subsystems/Trench.java
+++ src/main/java/frc/robot/subsystems/Trench.java
@@ -1,9 +1,21 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
+import frc.robot.config.ElectricalConstants;
+
 public class Trench {
 
+    private Servo servo;
+
     public Trench() {
-        
+        servo = new Servo(ElectricalConstants.ServoID);
     }
 
+    public void cameraRotation(boolean pressed) {
+        if (pressed){
+            servo.setPosition(90);
+        }else{
+            servo.setPosition(0);
+        }
+    }
 }
